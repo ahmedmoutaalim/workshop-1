787e183ff91ee96f0c8a1850e9b88318fa94d12b
ahmedmoutaalim <57999049+ahmedmoutaalim@users.noreply.github.com>
2020-03-13 10:22:22 +0100

    finalisation de Enoncé-1

---
+++ Enoncé-1/script.js
@@ -0,0 +1,23 @@
+
+var FavirtBook=["la raison","le popin"]
+function addFavirtBook(bookName){
+
+    var book =bookName.includes("great");
+    if (book != true) {
+
+     FavirtBook.push(bookName);
+
+    }
+}
+
+function printFavoritbook(){
+
+ console.log("number :" + FavirtBook.length)
+ console.log("favorit books : ")
+
+ for( let y=0 ; y<FavirtBook.length ;y++){
+
+    console.log(FavirtBook[y]);
+ }
+
+}
